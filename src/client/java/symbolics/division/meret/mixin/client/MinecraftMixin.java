13719746d0d86ef2d3568745ccf4d8fa65dd5b42
CallMeEchoCodes <romanbarrettsarpi@pm.me>
2024-11-26 15:27:53 +1100

    feat: get random song in tag

--- src/client/java/symbolics/division/meret/mixin/client/MinecraftMixin.java
+++ src/client/java/symbolics/division/meret/mixin/client/MinecraftMixin.java
@@ -5,6 +5,8 @@ import dev.doublekekse.area_lib.data.AreaClientData;
 import dev.doublekekse.area_lib.data.AreaSavedData;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderSet;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
@@ -17,6 +19,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Optional;
+
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
     @Shadow public LocalPlayer player;
@@ -26,16 +30,26 @@ public class MinecraftMixin {
             at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/Optionull;map(Ljava/lang/Object;Ljava/util/function/Function;)Ljava/lang/Object;")
     )
     public void modifyGetSituationalMusic(CallbackInfoReturnable<Music> ci) {
-        if (this.player != null) {
-            AreaSavedData.IdentifiableArea potentialArea = AreaClientData.getClientLevelData().find(this.player.level(), this.player.position());
-            Registry<JukeboxSong> songRegistry = this.player.level().registryAccess().registry(Registries.JUKEBOX_SONG).get();
+        if (this.player == null) return;
 
-            // I have this
-            ResourceLocation musicTypeTag = potentialArea.id();
-            // I need to get a random jukebox sound tagged with this
+        AreaSavedData.IdentifiableArea potentialArea = AreaClientData.getClientLevelData()
+                .find(this.player.level(), this.player.position());
+
+        if (potentialArea == null) return;
+
+        Registry<JukeboxSong> songRegistry =  this.player.level().registryAccess().registry(Registries.JUKEBOX_SONG).orElse(null);
+
+        if (songRegistry == null) return;
+
+        TagKey<JukeboxSong> musicTag = TagKey.create(
+                Registries.JUKEBOX_SONG,
+                potentialArea.id()
+        );
+
+        HolderSet.Named<JukeboxSong> taggedHolders = songRegistry.getOrCreateTag(musicTag);
+        Holder<JukeboxSong> song = taggedHolders.getRandomElement(this.player.getRandom()).orElse(null);
+        if (song == null) return;
 
 
-            
-        }
     }
 }
