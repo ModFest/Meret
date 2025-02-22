64d38862d42dda1476c1670c98d218a695e1bfe8
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2025-02-22 23:23:57 +0100

    Add silent area

--- src/client/java/symbolics/division/meret/MeretClient.java
+++ src/client/java/symbolics/division/meret/MeretClient.java
@@ -1,7 +1,9 @@
 package symbolics.division.meret;
 
 import com.mojang.datafixers.util.Pair;
+import dev.doublekekse.area_lib.Area;
 import dev.doublekekse.area_lib.data.AreaClientData;
+import dev.doublekekse.area_lib.data.AreaSavedData;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.Registry;
@@ -22,16 +24,23 @@ public class MeretClient implements ClientModInitializer {
 	public static Music getOverride(LocalPlayer player) {
 		if (player == null) return null;
 
-		Registry<JukeboxSong> songRegistry =  player.level().registryAccess().registry(Registries.JUKEBOX_SONG).orElse(null);
+		Registry<JukeboxSong> songRegistry = player.level().registryAccess().registry(Registries.JUKEBOX_SONG).orElse(null);
 
 		if (songRegistry == null) {
 			Meret.LOGGER.info("Could not get a handle on songRegistry, you will not hear any music.");
 			return null;
 		}
 
+		AreaSavedData areaSavedData = AreaClientData.getClientLevelData();
+		Area silentArea = areaSavedData.get(Meret.id("silent"));
+
+		if(silentArea != null && silentArea.contains(player)) {
+            return null;
+		}
+
 		// sorry jasmine  (´・ω・`)
 		return songRegistry.getTagNames()
-                .map(tag -> new Pair<>(tag, AreaClientData.getClientLevelData().get(tag.location())))
+                .map(tag -> new Pair<>(tag, areaSavedData.get(tag.location())))
                 .filter(p -> p.getSecond() != null && p.getSecond().contains(player))
 				.max(Comparator.comparingInt(p -> p.getSecond().getPriority()))
 				.flatMap(p -> songRegistry.getOrCreateTag(p.getFirst()).getRandomElement(player.getRandom()))
