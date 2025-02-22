40c9d52e93beb6698e138590e22d1a8e068e2b73
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2025-02-23 00:39:22 +0100

    Remove comments and unused code

--- src/client/java/symbolics/division/meret/MeretClient.java
+++ src/client/java/symbolics/division/meret/MeretClient.java
@@ -19,7 +19,6 @@ public class MeretClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
     }
 
     @Nullable
--- src/main/java/symbolics/division/meret/Meret.java
+++ src/main/java/symbolics/division/meret/Meret.java
@@ -11,25 +11,12 @@ import org.slf4j.LoggerFactory;
 
 public class Meret implements ModInitializer {
     public static final String MOD_ID = "meret";
-    public static final String MF_ID = "mf121_music";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static final AreaDataComponentType<AreaMusicDelay> AREA_MUSIC_DELAY_DATA_COMPONENT = AreaDataComponentTypeRegistry.register(id("area_delay"), AreaMusicDelay::new);
 
     public static ResourceLocation id(String id) {
         return ResourceLocation.fromNamespaceAndPath(MOD_ID, id);
-    }
-
-    public static final class OVERRIDES {
-//		public static final ResourceLocation STONE_ID = ResourceLocation.fromNamespaceAndPath(MF_ID, "override.industrial");
-//		public static final SoundEvent STONE = SoundEvent.createVariableRangeEvent(STONE_ID);
-//		public static final ResourceLocation IRON_ID = ResourceLocation.fromNamespaceAndPath(MF_ID, "override.industrial");
-//		public static final SoundEvent IRON = SoundEvent.createVariableRangeEvent(IRON_ID);
-//		public static final ResourceLocation INDUSTRIAL_ID = ResourceLocation.fromNamespaceAndPath(MF_ID, "override.industrial");
-//		public static final SoundEvent INDUSTRIAL = SoundEvent.createVariableRangeEvent(INDUSTRIAL_ID);
-//		public static final ResourceLocation INFO_ID = ResourceLocation.fromNamespaceAndPath(MF_ID, "override.industrial");
-//		public static final SoundEvent INFO = SoundEvent.createVariableRangeEvent(INFO_ID);
-
     }
 
     @Override
@@ -39,13 +26,5 @@ public class Meret implements ModInitializer {
                 MeretCommand.register(dispatcher);
             }
         );
-
-        // This code runs as soon as Minecraft is in a mod-load-ready state.
-        // However, some things (like resources) may still be uninitialized.
-        // Proceed with mild caution.
-
-//		Registry.register(BuiltInRegistries.SOUND_EVENT, OVERRIDES.INDUSTRIAL_ID, OVERRIDES.INDUSTRIAL);
-//		Registry.register(Registries.SOUND_EVENT, OVERRIDES.INDUSTRIAL_ID, OVERRIDES.INDUSTRIAL);
-
     }
 }
