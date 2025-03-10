e706120df2b9cd536b0181b3b943c63e4689fdf3
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2025-03-10 17:36:36 +0100

    Use vanilla song delay

--- src/client/java/symbolics/division/meret/mixin/client/MusicManagerMixin.java
+++ src/client/java/symbolics/division/meret/mixin/client/MusicManagerMixin.java
@@ -1,16 +1,12 @@
 package symbolics.division.meret.mixin.client;
 
-import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
-import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.client.sounds.MusicManager;
 import net.minecraft.sounds.Music;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -20,24 +16,10 @@ public class MusicManagerMixin {
     @Shadow
     @Final
     private RandomSource random;
-    @Shadow private @Nullable SoundInstance currentMusic;
-    @Unique
-    private int actualNextSongDelay;
-
-    @Inject(method = "tick", at = @At("HEAD"))
-    void tick(CallbackInfo ci) {
-        if(currentMusic == null) {
-            actualNextSongDelay--;
-        }
-    }
-
-    @WrapWithCondition(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/sounds/MusicManager;startPlaying(Lnet/minecraft/sounds/Music;)V"))
-    boolean startPlayingCondition(MusicManager instance, Music music) {
-        return actualNextSongDelay <= 0;
-    }
+    @Shadow private int nextSongDelay;
 
     @Inject(method = "startPlaying", at = @At("HEAD"))
     void startPlaying(Music music, CallbackInfo ci) {
-        actualNextSongDelay = Mth.nextInt(random, music.getMinDelay(), music.getMaxDelay());
+        nextSongDelay = Mth.nextInt(random, music.getMinDelay(), music.getMaxDelay());
     }
 }
